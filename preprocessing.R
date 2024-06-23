7d9b5541e063b8805b1f78858f11edc3331e5765
Patrick Greber <grebepat@students.zhaw.ch>
2024-06-23 16:29:28 +0200

    Dem Ende nähern

--- preprocessing.R
+++ preprocessing.R
@@ -1,8 +1,95 @@
+
+
+## Load libraries
+  
+options(repos = c(CRAN = "https://cran.rstudio.com"))
+
+
+install.packages("pacman")
+library("pacman")
+
+install.packages("markdown")
+library("markdown")
+
+
+## loading necessary libraries
+
+library("ggh4x")
+library("dplyr")
+library("ggplot2")
+library("tidyr")
+library("sf")
+library("sp")
+library("terra")
+library("tmap")
+library("zoo")
+library("tidyverse")
+library("leaflet")
+library("XML")
+library("lubridate")
+library("forcats")
+library("httr")
+library("hereR")
+library("png")
+library("gridExtra")
+library("RColorBrewer")
+library("purrr")
+library("ggridges")
+library("agricolae")
+
+
+
+## Import Spatial Data
+# Import spatial data. Geopackage was preprocessed using Quantum GIS Version 3.34.5.
+
+## All Spatial Data is stored in a geopackage called basic_data. Basic_data consists of several layers such as the street network, surface type or housing footprint. The layers were preprocessed and cliped to the extent of the city outline of zurich using Quantum GIS Version 3.34.5
+
+
+## Show layers in basic_data.gpkg
+sf::st_layers("gis_files/basic_data.gpkg")
+
+
+## Import street network from zurich, based on the swisstlm3d
+streets <- sf::read_sf("gis_files/basic_data.gpkg", "street_network_z") |> 
+  select(objektart, geom) |> 
+  mutate(
+    objektart = as.factor(objektart),
+    width = as.numeric(substr(objektart, start = 1, stop = 1)),
+  ) |> 
+  na.omit()
+
+
+## Import city border of zurich
+outline <- sf::read_sf("gis_files/basic_data.gpkg", "city_outline")
+
+
+## Import housing footprint of zurich
+housing <- sf::read_sf("gis_files/basic_data.gpkg", "housing_footprint") |> 
+  select(objektart, geom) |>
+  mutate(
+    objektart = as.factor(objektart)
+  )
+
+
+## Import surface type of zurich
+surface <- sf::read_sf("gis_files/basic_data.gpkg", "surface_type") |> 
+  select(art, geom) |> 
+  mutate(
+    art = as.factor(art)) |> 
+  filter(art == "fliessendes Gewässer" | art == "stehendes Gewässer" | art == "Strasse, Weg" | art == "Verkehrsinsel") |> 
+  na.omit()
+
+
+## Import digital height model DHM25, a set of data representing the 3D form of the earth’s surface without vegetation and buildings
+height <- terra::rast("gis_files/dhm25_zh.tif")
+
+
+
+## 1. Generating alternative routes using hereR
+
 # Creating alternative routes
-
-load("C:/Users/patri/Desktop/Semesterproject/semesterproject/all_routes_seg_tunnel_cor.rda")
-
-
+## load all_routes_seg_tunnel_cor from global environment
+load("rda_files/all_routes_seg_tunnel_cor.rda")
 
 
 ## Extract starting and endpoints from real world trajecotries
@@ -21,13 +108,14 @@ tm_shape(a_to_b) +
 
 ## Get accsess to API from here, this API will later generate our alternative routes based on the start and endpoints of the deliveries
 library("hereR")
-set_key("PARpVmlSSQAeuGjMhIdg94LMi9s1ha4UShazq0j8sAo")
+set_key("qCKNngXWAlCW3GjOMYSldRSI_LwQ-O0PxOjPUuQPXgc")
 
-
-############### ATTENTION: We get a lot of warning due to our limited access to this API. We're barely able to get 1 alternative route per delivery for 1 shift per messenger. We have access to almost 60 shifts per messenger, but can not generate an alternative for one shift per messenger, major bummer! We try to run the function (route) twice, ones for raeubertochter, ones for donner so at least for day one, all routes get an alternative
 
 ## Create a dataframe with even rows -> ENDPOINTS
 df_even <- a_to_b[seq(2, nrow(a_to_b), by = 2), ]
+df_even <- df_even |> 
+  filter(shift == "F1")
+
 # df_even_raeubertochter <- filter(df_even, messenger == "raeubertochter")
 # df_even_donner <- filter(df_even, messenger == "donner")
 
@@ -37,6 +125,9 @@ df_even <- a_to_b[seq(2, nrow(a_to_b), by = 2), ]
 
 ## Create a dataframe with odd rows -> STARTING POINTS
 df_odd <- a_to_b[seq(1, nrow(a_to_b), by = 2), ]
+df_odd <- df_odd |> 
+  filter(shift == "F1")
+
 # df_odd_raeubertochter <- filter(df_odd, messenger == "raeubertochter")
 # df_odd_donner <- filter(df_odd, messenger == "donner")
 
@@ -45,7 +136,7 @@ df_odd <- a_to_b[seq(1, nrow(a_to_b), by = 2), ]
 testruns <- nrow(df_even)
 
 
-alternates <- map(seq_len(testruns), function(x){
+alternates_final <- map(seq_len(testruns), function(x){
   alternative_lines = route(origin = df_odd[x,], destination = df_even[x,], transport_mode = "bicycle", traffic = FALSE, results = 7 , routing_mode = "short")
   Sys.sleep(1.2)
   segment_id = df_even$segment_id_cor[x]
@@ -53,47 +144,34 @@ alternates <- map(seq_len(testruns), function(x){
   alternative_lines
 })
 
-?route
- 
-# alternative_lines <- route(origin = df_odd[1,], destination = df_even[1,], transport_mode = "bicycle", traffic = FALSE, results = 7 , routing_mode = "short")
+save(alternates_final, file = "rda_files/alternates_final.rda")
 
 
 
-## create an alternative route for every single delivery from raeubertochter
-# alternative_lines_donner <- route(origin = df_odd_donner, destination = df_even_donner, transport_mode = "bicycle", traffic = FALSE, results = 1 , routing_mode = "short")
-# 
-# ## create an alternative route for every single delivery from raeubertochter
-# alternative_lines_raeubertochter <- route(origin = df_odd_raeubertochter, destination = df_even_raeubertochter, transport_mode = "bicycle", traffic = FALSE, results = 1 , routing_mode = "short")
+df <- enframe(alternates_final)
+df <-  st_as_sf(do.call(rbind, alternates_final))
 
-
-############### We still get warning because we sent too many request in a given time frame. We will compute the alternatives with a little pause in between so we can get alternatives for all routes on day one.
-
-
-## Combine datasets back together
-#alternative <- rbind(alternative_lines_donner, alternative_lines_raeubertochter)
 
 
 ## Transform alternative in spatial point dataframe
-alternates2  <- tidyr::unnest(alternates, lines)
-df <- enframe(alternates)
-df <-  st_as_sf(do.call(rbind, alternates))
-
-
-merged_dataset <- df %>%
-  group_by(segment_id, rank) %>%
+merged_dataset <- df |> 
+  group_by(segment_id, rank) |> 
   summarise(geometry = sf::st_union(geometry))
 
 alternative <- merged_dataset |> 
   st_transform(crs = 2056) |> 
   st_cast("POINT") |> 
   st_zm(drop = TRUE) |> 
-  rename(segment_id = segment_id)
+  rename(segment_id = segment_id) |> 
+  mutate(
+    segment_id = as.factor(segment_id),
+    rank = as.factor(rank)
+  )
 
 
 ## drop height information, well add id back later via dhm25
-alternative <- st_zm(alternative, drop = TRUE)
+alternative_final <- st_zm(alternative, drop = TRUE)
 
-
-
-save(alternative, file = "alternative.rda")
+# save robject
+save(alternative_final, file = "rda_files/alternative_final.rda")
 
